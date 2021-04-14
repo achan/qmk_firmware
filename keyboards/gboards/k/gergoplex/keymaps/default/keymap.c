a35813f4741d7c28854924d1f5b5b08b0e974437
Amos Chan <ac@amoschan.com>
2021-04-14 12:39:00 -0400

    Update keymap

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_gergoplex(
     KC_Q, KC_W, KC_E, KC_R, KC_T,                       KC_Y, KC_U, KC_I, KC_O, KC_P, 
-    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_LCTL, KC_SCLN),
+    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_RCTL, KC_SCLN),
     MT(MOD_LSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,  	KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
 
  		MT(MOD_LALT, KC_NO), MT(MOD_LGUI, KC_NO), LT(SYMB, KC_SPC),									// Left
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbols layer
  *
  * ,-----------------------------.      ,-----------------------------.
- * |  !  |  @  |  #  |  $  |  ^  |      |  &  |  *  |  ~  |  -  |  \  |
+ * |  !  |  @  |  #  |  $  |  ^  |      |  &  |  *  |  `  |  -  |  \  |
  * |-----------------------------|      |-----------------------------|
- * |  [  |  ]  |  (  |  )  |     |      |  +  |  -  |  =  |     |  |  | 
+ * |  [  |  ]  |  (  |  )  |  %  |      |  +  |  -  |  =  |     |  |  | 
  * |-----------------------------|      |-----------------------------|
  * |     |     |  {  |  }  |     |      |     |     |     |     |     |
  * `-----------------------------'      `-----------------------------'
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-------------------------'           '-----------------' 
  */
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_CIRC,      KC_AMPR, KC_ASTR, KC_TILD, KC_MINS, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TRNS,      KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_CIRC,      KC_AMPR, KC_ASTR,  KC_GRV, KC_MINS, KC_BSLS,
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_PERC,      KC_PLUS, KC_MINS,  KC_EQL, KC_TRNS, KC_PIPE,
     KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  
 
                       KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUMB] = LAYOUT_gergoplex(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,  	 KC_7, 	  KC_8,    KC_9,    KC_0,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,	KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
     )
