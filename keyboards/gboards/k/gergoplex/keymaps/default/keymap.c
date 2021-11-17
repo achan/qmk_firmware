fcd4f0326b492858a8b6da8c819437145c01be4c
Amos Chan <ac@amoschan.com>
2021-11-17 17:01:27 -0500

    More changes

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -31,63 +31,64 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
- * ,------------------------------.      ,--------------------------------.
- * |    Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |    P   |
- * |------+-----+-----+-----+-----|      |--------------------------------|
- * |CTRL/A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  | CTRL/; | 
- * |------+-----+-----+-----+-----|      |--------------------------------|
- * |SHFT/Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  | SHFT/? |
- * `------+-----+-----+-----+-----'      `--------------------------------'
- *  .-------------------------.           .-----------------.   
- *  |ESC/ALT|BSPC/META|SPC(SYM)|           |SPC(NUM)|HYPR|MEH|
- *  '-------------------------'           '-----------------' 
+ * ,-----------------------------.      ,------------------------------------.
+ * |  Q  |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |   P        |
+ * |-----+-----+-----+-----+-----|      |------------------------------------|
+ * |  A  |  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  |   "        | 
+ * |-----+-----+-----+-----+-----|      |------------------------------------|
+ * |  Z  |  X  |  C  |  V  |  B  |      |  N  |  M  |  ,  |  .  | CTRL/ENTER |
+ * `-----+-----+-----+-----+-----'      `------------------------------------'
+ *      .----------------------.               .-------------------.   
+ *      | CMD | SYM | SPC(HYP) |               | SHIFT | NAV | OPT |
+ *      '----------------------'               '-------------------' 
  */
 [BASE] = LAYOUT_gergoplex(
-    KC_Q, KC_W, KC_E, KC_R, KC_T,                       KC_Y, KC_U, KC_I, KC_O, KC_P, 
-    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,  	KC_H, KC_J, KC_K,    KC_L, 	 MT(MOD_LCTL, KC_SCLN),
-    MT(MOD_LSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,  	KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
+    KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I,    KC_O,   KC_P, 
+    KC_A, KC_S, KC_D, KC_F, KC_G,  	    KC_H, KC_J, KC_K,    KC_L, 	 KC_QUOT,
+    KC_Z, KC_X, KC_C, KC_V, KC_B,  	    KC_N, KC_M, KC_COMM, KC_DOT, CTL_T(KC_ENT),
 
- 		MT(MOD_LALT, KC_NO), MT(MOD_LGUI, KC_NO), LT(SYMB, KC_SPC),									// Left
-		LT(NUMB, KC_ENT), KC_HYPR, KC_MEH
+ 		KC_LGUI, LT(SYMB, KC_NO), HYPR_T(KC_SPC), // Left
+		MT(MOD_RSFT, KC_NO), LT(NUMB, KC_NO), KC_LALT   // Right
     ),
 
 /* Keymap 1: Symbols layer
  *
  * ,-----------------------------.      ,-----------------------------.
- * |  !  |  @  |  #  |  $  |  ^  |      |  &  |  *  |  ~  |  -  |  \  |
+ * |  '  |  "  |  ^  |  ?  |  `  |      |  [  |  <  |  =  |  >  |  ]  |
  * |-----------------------------|      |-----------------------------|
- * |  [  |  ]  |  (  |  )  |     |      |  +  |  -  |  =  |     |  |  | 
+ * |  !  |  @  |  #  |  $  |  %  |      |  {  |  (  |  :  |  )  |  }  | 
  * |-----------------------------|      |-----------------------------|
- * |     |     |  {  |  }  |     |      |     |     |     |     |     |
+ * |  \  |  ~  |  |  |  &  |  ;  |      |  /  |  *  |  -  |  +  |  _  |
  * `-----------------------------'      `-----------------------------'
  *  .-------------------------.           .-----------------.   
  *  |ESC/ALT|BSPC/META|SPC(SYM)|           |SPC(NUM)|HYPR|MEH|
  *  '-------------------------'           '-----------------' 
  */
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_CIRC,      KC_AMPR, KC_ASTR, KC_TILD, KC_MINS, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TRNS,      KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  
+    KC_QUOT, KC_DQUO, KC_CIRC, KC_QUES, KC_GRAVE,     KC_LBRC, KC_LT,   KC_EQL,  KC_GT,   KC_RBRC,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_LCBR, KC_LPRN, KC_COLN, KC_RPRN, KC_RCBR,
+    KC_BSLS, KC_TILD, KC_PIPE, KC_AMPR, KC_SCLN,      KC_SLSH, KC_ASTR, KC_MINS, KC_PLUS, KC_UNDS,  
 
                       KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Keymap 2: Pad/Function layer
- * ,-----------------------------.       ,-------------------------------.
- * |  1  |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
- * |-----+-----+-----+-----+-----|      |-------------------------------|
- * |     |     |     |     |     |      | LFT | DWN | UP  | RGT | VOLUP | 
- * |-----+-----+-----+-----+-----+      |-------------------------------|
- * |     |     |     |     |     |      |MLFT | MDWN| MUP | MRGT| VOLDN |
- * `-----+-----+-----+-----+-----'     	`-------------------------------'
+ * ,-----------------------------.      ,------------------------------------.
+ * | CTRL | OPT |    | TAB |     |      | PGUP | HOME | END | PGDOWN | VOLUP |
+ * |-----+-----+-----+-----+-----|      |------------------------------------|
+ * |  1  |  2  |  3  |  4  |  5  |      | LFT  | DWN  | UP  | RGT    | VOLDN | 
+ * |-----+-----+-----+-----+-----+      |------------------------------------|
+ * |  6  |  7  |  8  |  9  |  0  |      | ESC  |      |     |        | MUTE  |
+ * `-----+-----+-----+-----+-----'     	`------------------------------------'
  *          .-----------------.           .-----------------.   
  *          |     |    |      |           |     | PLY | SKP |
  *          '-----------------'           '-----------------' 
  */
 [NUMB] = LAYOUT_gergoplex(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,  	 KC_7, 	  KC_8,    KC_9,    KC_0,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,	KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS,
+    KC_LCTL, KC_LOPT, KC_TRNS, KC_TAB,  KC_TRNS,        KC_PGUP, KC_HOME, KC_END,  KC_PGDN, KC_VOLU,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD,
+    KC_6,  	 KC_7, 	  KC_8,    KC_9,    KC_0,   	   KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,
+
                       KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
